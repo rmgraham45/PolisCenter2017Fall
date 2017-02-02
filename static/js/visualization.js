bfbdf8019ac9d0c75c10386a10ee64b4d5b6ee02
michael ketiku <mketiku@gmail.com>
2017-02-01 21:49:42 -0500

    Update to V4

--- static/js/visualization.js
+++ static/js/visualization.js
@@ -1,12 +1,18 @@
 //************************************************************
-// Initialize Global Variables and call methods to plot visualiization 
+// Initialize Global Variables and call methods to plot visual
 //************************************************************
 
 
-//margin variable is used to give margin to the svg element.USing this you can design relative position code
-var margin = { top: 20, right: 10, bottom: 100, left: 60 }, pad = 20,
+//margin variable is used to give margin to the svg element. Using this you can design relative position code
+var margin = {
+		top: 20,
+		right: 10,
+		bottom: 100,
+		left: 60
+	},
+	pad = 20,
 
-	//This is to get width and height from parentdiv.So graph will be placed inside parent div
+	//This is to get width and height from parentdiv. So graph will be placed inside parent div
 	width = document.getElementById("visualization").offsetWidth - margin.left - margin.right,
 	height = document.getElementById("visualization").offsetHeight - margin.top - margin.bottom,
 	radius = height / 2 - margin.top;
@@ -15,22 +21,26 @@ var svg = d3.select("#visualization").append("svg")
 	.attr("height", height + margin.top + margin.bottom)
 	.append("g")
 	.attr("transform",
-	"translate(" + (radius + margin.left) + "," + height / 2 + ")");
+		"translate(" + (radius + margin.left) + "," + height / 2 + ")");
 
 //************************************************************
 // Donut chart D3 config
 //************************************************************
 
-var arc = d3.svg.arc()
+var arc = d3.arc()
 	.innerRadius(0)
 	.outerRadius(radius);
 
-var pie = d3.layout.pie().sort(null)
+var pie = d3.pie().sort(null)
 	.value(function (d) {
+		console.log('ok')
 		return d.data1;
-	}).padAngle(0);;
-d3.xml("data/religiontree.xml", function (error, data) {
-	if (error) { throw error; alert("Unable to load XML file") };
+	});
+d3.xml("religiontree.xml", function (error, data) {
+	if (error) {
+		throw error;
+		alert("Enable to load XML file")
+	};
 	console.log(data);
 
 	// Convert the XML document to an array of objects.
@@ -49,46 +59,59 @@ d3.xml("data/religiontree.xml", function (error, data) {
 	// Add SVG and draw donut chart using config
 	//************************************************************
 
-	var tooltip = d3.select("body").append("div");// declare the tooltip div
-	console.log(data);
+	var tooltip = d3.select("body").append("div"); // declare the tooltip div
+
 	var donut = svg.selectAll("path")
 		.data(pie(data))
 		.enter();
-
+	console.log(donut);
 	donut.append("path")
 		.on("mouseover", function (d) {
 			tooltip.attr("class", "tooltip");
 			tooltip.style("left", d3.event.pageX + 10 + "px");
 			tooltip.style("top", d3.event.pageY - 25 + "px");
 			tooltip.style("display", "inline-block");
-			tooltip.html("<div class='tooltip-title'>" + d.data.label
-				+ "</div>" + "<div class='tooltip-count'>  " + d.data.data1 + "</div>");
+			tooltip.html("<div class='tooltip-title'>" + d.data.label +
+				"</div>" + "<div class='tooltip-count'>  " + d.data.data1 + "</div>");
 		})
 		.on("mouseout", function (d) {
 			tooltip.style("display", "none");
 		})
-		.attr("fill", function (d, i) { return "#" + d.data.color.substring(2, 8); })
+		.attr("fill", function (d, i) {
+			return "#" + d.data.color.substring(2, 8);
+		})
 		.transition()
-		.ease("exp")
+
 		.duration(1000)
-		.attrTween("d", tweenPie);
+		.attrTween('d', tweenPie)
 
 	//************************************************************
 	// Add Text on arc of circle and
 	// at bottom show total no of books count
 	//************************************************************
 	donut.append("text")
-		.attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("transform", function (d) {
+			return "translate(" + arc.centroid(d) + ")";
+		})
 		.transition()
 		.delay(1200)
-		.attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
+		.attr("transform", function (d) {
+			return "translate(" + arc.centroid(d) + ")";
+		})
 		.attr("dy", ".35em")
 		.attr("dx", "-1em")
-		.text(function (d) { if (d.data.data1 > 3) return d.data.data1 + " %"; });
+		.text(function (d) {
+			if (d.data.data1 > 3) return d.data.data1 + " %";
+		});
 
 	function tweenPie(b) {
-		var i = d3.interpolate({ startAngle: 1.1 * Math.PI, endAngle: 1.1 * Math.PI }, b);
-		return function (t) { return arc(i(t)); };
+		var i = d3.interpolate({
+			startAngle: 1.1 * Math.PI,
+			endAngle: 1.1 * Math.PI
+		}, b);
+		return function (t) {
+			return arc(i(t));
+		};
 	}
 
 	//************************************************************
@@ -100,7 +123,7 @@ d3.xml("data/religiontree.xml", function (error, data) {
 		.data(data)
 		.enter().append("g")
 		.attr("transform", function (d, i) {
-			return "translate(" + (radius + pad) + "," + ((i * 35) - height / 2.5 - pad) + ")"; // place each legend on the right 
+			return "translate(" + (radius + pad) + "," + ((i * 35) - height / 2.5 - pad) + ")"; // place each legend on the right
 		})
 		.attr("class", "legend");
 
@@ -120,5 +143,3 @@ d3.xml("data/religiontree.xml", function (error, data) {
 
 
 });
-
-
