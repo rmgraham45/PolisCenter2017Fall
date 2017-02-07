07711b13f21d1fe9a2079aa4907d72932026bf82
michael ketiku <mketiku@gmail.com>
2017-02-06 22:47:14 -0500

    Add getCensusType function to pull dropdown information from xml into form

--- src/static/js/data.js
+++ src/static/js/data.js
@@ -1,5 +1,7 @@
 $(document).ready(() => {
   getYears();
+  getCensusType();
+
   $('#map-info').on('submit', (e) => {
     e.preventDefault();
     let geog,
@@ -19,7 +21,7 @@ $(document).ready(() => {
     parameters = {
       geog: $('input:radio[name=opt-map-geography]:checked').val(),
       rcvi: '4',
-      DominantReligion: $('input:radio[name=opt-map-type]:checked').val(),
+      DominantReligion: $('input:radio[name=opt-map-representation]:checked').val(),
       dy: $('#sel3').val(),
       glid: '011',
       dy2: $('#sel3').val(),
@@ -29,11 +31,18 @@ $(document).ready(() => {
       DenomFamily: '0',
       DenomFamily2: '0',
       FromPie: 'true',
-
+      // $('input[name="opt-map-representation"]:checked').val()
     };
     $form = $('form.map-info');
     parameters = $.param(parameters);
     const url = 'https://in-polis-app28.ads.iu.edu/daarws/GetTreeMapData.aspx?';
+    deleteSvg();
     visualizePieChart(url + parameters);
   });
+
+  // $('#sel1').change(function () {
+  //   // alert($(this).val());
+  //   const name = $('#sel1 option:selected').text();
+  //   getCensusType();
+  // });
 });
--- src/static/js/utils.js
+++ src/static/js/utils.js
@@ -3,7 +3,6 @@ function getYears() {
   $.ajax({
     // Get list of applicable years and appends the value to dropdown menu
     type: 'GET',
-    // url: cors_proxy + url,
     url,
     success(xml) {
       $(xml).find('ReligionCensusYear').each(function () {
@@ -12,4 +11,44 @@ function getYears() {
       });
     },
   });
+}
+
+function getCensusType() {
+  const url = 'https://in-polis-app28.ads.iu.edu/daarws/religioncensusvariables.xml';
+  $.ajax({
+    // Get list of applicable years and appends the value to dropdown menu
+    type: 'GET',
+    url,
+    success(xml) {
+      // console.log(xml)
+
+      $(xml)
+        .find('ReligionCensusYear')
+        .first()
+        .next()
+        .find('ReligionCensusVariable')
+        .each(function () {
+          const $this = $(this);
+          typeLabel = $this.attr('label');
+          typeData = $this.attr('data');
+          $('#sel1').append(`<option value='${typeData}'>${typeLabel}</option>`);
+          $this.children()
+            .each(function () {
+              percentageLabel = $(this).attr('label');
+              percentageData = $(this).attr('data');
+              $('#sel2').append(`<option value='${percentageData}'>${percentageLabel}</option>`);
+            });
+          // const year = $(this).attr('data');
+          // $('#sel3').append(`<option value='${year}'>${year}</option>`);
+        });
+    },
+    error(error) {
+      // alert(error);
+      console.log(error);
+    },
+  });
+}
+
+function deleteSvg() {
+  $('svg').remove();
 }
