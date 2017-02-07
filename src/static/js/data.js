c7537e92e2383875e9ebe361f40312bafec70c97
michael ketiku <mketiku@gmail.com>
2017-02-06 19:28:38 -0500

    Update eslint and add utils file

--- src/static/js/data.js
+++ src/static/js/data.js
@@ -1,6 +1,5 @@
-const cors_proxy = 'https://crossorigin.me/';
-// cors proxy to avoid access-control-allow-origin-header issues
 $(document).ready(() => {
+  getYears();
   $('#map-info').on('submit', (e) => {
     e.preventDefault();
     let geog,
@@ -32,44 +31,9 @@ $(document).ready(() => {
       FromPie: 'true',
 
     };
-    console.log(parameters);
     $form = $('form.map-info');
     parameters = $.param(parameters);
     const url = 'https://in-polis-app28.ads.iu.edu/daarws/GetTreeMapData.aspx?';
-    // $.ajax({
-    //   type: 'GET',
-    //   url: url + parameters,
-    //   success(result) {
-    //     console.log('success');
-    //     console.log(result);
-    //     visualizePieChart(result);
-    //   },
-    //   error(error) {
-    //     console.log(error);
-    //   },
-    // });
-    visualizePieChart(url+ parameters)
+    visualizePieChart(url + parameters);
   });
-
-  function getYears() {
-    const url = 'https://in-polis-app28.ads.iu.edu/daarws/religioncensusvariables.xml';
-    $.ajax({
-            // Get list of applicable years and appends the value to dropdown menu
-      type: 'GET',
-      // url: cors_proxy + url,
-      url: url,
-      success(xml) {
-        $(xml).find('ReligionCensusYear').each(function () {
-          const year = $(this).attr('data');
-          $('#sel3').append(`<option value='${year}'>${year}</option>`);
-        });
-      },
-    });
-  }
-  getYears();
-
-
-  // d3.request(url)
-  //     .get(data, function(result){
-  // console.log(result.response)});
 });
