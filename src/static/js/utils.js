73b7a01a6dd6a5d7b4bcdcc3d0e5c91d195acf38
michael ketiku <mketiku@gmail.com>
2017-02-07 23:11:55 -0600

    Add getCensusPercentage function to populate dropdown intelligently

--- src/static/js/utils.js
+++ src/static/js/utils.js
@@ -32,14 +32,32 @@ function getCensusType() {
           typeLabel = $this.attr('label');
           typeData = $this.attr('data');
           $('#sel1').append(`<option value='${typeData}'>${typeLabel}</option>`);
-          $this.children()
-            .each(function () {
-              percentageLabel = $(this).attr('label');
-              percentageData = $(this).attr('data');
-              $('#sel2').append(`<option value='${percentageData}'>${percentageLabel}</option>`);
-            });
-          // const year = $(this).attr('data');
-          // $('#sel3').append(`<option value='${year}'>${year}</option>`);
+        });
+    },
+    error(error) {
+      // alert(error);
+      console.log(error);
+    },
+  });
+}
+
+function getCensusPercentage(name) {
+  const url = 'https://in-polis-app28.ads.iu.edu/daarws/religioncensusvariables.xml';
+  $.ajax({
+    // Get list of applicable years and appends the value to dropdown menu
+    type: 'GET',
+    url,
+    success(xml) {
+      // console.log(xml)
+
+      $(xml)
+        .find('ReligionCensusYear:first ReligionCensusVariable[label="' + name + '"] NormalizationVariable')
+        .each(function () {
+          percentageLabel = $(this).attr('label');
+          percentageData = $(this).attr('data');
+          console.log(percentageData);
+          console.log(percentageLabel);
+          $('#sel2').append(`<option value='${percentageData}'>${percentageLabel}</option>`);
         });
     },
     error(error) {
